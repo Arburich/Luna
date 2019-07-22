f952adef508858780a04482929720f3568bc74ab
Arburich <ltmonast@hotmail.com>
2019-07-21 18:11:59 -0700

    cleaning up the repo for a clean push

--- events/guildCreate.js
+++
@@ -1,5 +0,0 @@
-// This event executes when a new guild (server) is joined.
-
-module.exports = (client, guild) => {
-  client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
-};
