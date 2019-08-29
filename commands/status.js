996c72fc0b66982da26de950b1cb0dac0be90c38
Arburich <ltmonast@hotmail.com>
2019-08-29 16:02:02 -0700

    new status messages arent going to work

--- commands/status.js
+++ commands/status.js
@@ -1,48 +1,21 @@
 const { version } = require("discord.js");
 const moment = require("moment");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 require("moment-duration-format");
 
-var ms = require('../modules/minestat');
-  var MCserver = "check failed"
-  ms.init('minecraft.alchemyden.com', 25565, function(result)
-{
-  //message.channel.send("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
-  if(ms.online)
-  {
-    MCserver = "Online"
-    //message.channel.send("Server is online running version " + ms.version + " with " + ms.current_players + " out of " + ms.max_players + " players.");
-    //message.channel.send("Message of the day: " + ms.motd);
-    //message.channel.send("Latency: " + ms.latency + "ms");
-  }
-  else
-  {
-    //message.channel.send("Server is offline!");
-    MCserver = "Offline"
-  }
-});
-var ping = require('ping');
-var website = "Check Failed"
-var hosts = ['google.com'];
-hosts.forEach(function(host){
-    ping.sys.probe(host, function(isAlive){
-        website = isAlive ? "Online" : "Offline";
-    });
-});
-
 exports.run = (client, message, args, level) => { 
-  
-  
   
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
   message.channel.send(`= Luna Status =
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Uptime     :: ${duration}
 • Users      :: ${client.users.size.toLocaleString()}
-• Minecraft  :: ${MCserver}
-• Website    :: ${website}
 • Github     :: https://github.com/Arburich/Luna
 = Luna was created by Arburich <3. Thanks Luna! =`, {code: "asciidoc"});
 };
+// • Minecraft  :: ${MCserver}
+// • Website    :: ${website}
+
 //• Servers    :: ${client.guilds.size.toLocaleString()}
 //• Channels   :: ${client.channels.size.toLocaleString()}
 //• Discord.js :: v${version}
